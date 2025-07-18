c408ab238fe44f9657d355e2e32df4f58977c99f
Jorge Rios <jorgerios47@gmail.com>
2025-07-18 10:20:44 -0500

    added a post request that increases the calories consumed

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+app.set("view engine", "ejs")
+const port = 3000
+
+app.use(express.urlencoded({ extended: true })) // Add body parser for form data
+
+let caloriesConsumed = 0 // Store calories in memory
+
+app.get('/', (req, res) => {
+    res.render("index.ejs", {
+        caloriesConsumed
+    })
+})
+
+app.post('/add-calories', (req, res) => {
+    const calsInputed = parseInt(req.body.calories)
+    if (!isNaN(calsInputed)) {
+        caloriesConsumed += calsInputed
+    }
+    res.redirect('/')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
