a76587a18053f3d8eea398aadf92dfb4543076e4
Jorge Rios <jorgerios47@gmail.com>
2025-07-27 15:40:47 -0500

    didnt work

--- server.js
+++ server.js
@@ -133,16 +133,10 @@ app.post('/log-in', async (req, res) => {
         asResponse: true
     });
 
-    if (data.status >= 200 && date.status < 300) {
-        const setCookiesHeader = data.headers.get('set-cookie')
-        res.set('set-cookie', setCookiesHeader)
+    const setCookiesHeader = data.headers.get('set-cookie')
+    res.set('set-cookie', setCookiesHeader)
 
-        res.redirect('/')
-    } else {
-        res.status(401).render('login', {
-            error: 'Invalid username or password'
-        })
-    }
+    res.redirect('/')
 
 })
 
