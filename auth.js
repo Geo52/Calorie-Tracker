bf22f6fd0ace7e105798f3f348ad1c008b866ea3
Jorge Rios <jorgerios47@gmail.com>
2025-08-03 13:34:38 -0500

    railway configuration

--- auth.js
+++ auth.js
@@ -9,6 +9,9 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT || 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 export const auth = betterAuth({
